35757070933c24042cd06d7c714e8c52b874bad8
Elier25 <elier.aries@gmail.com>
2019-11-09 17:39:02 -0600

    Add files via upload

---
+++ includes/menu.php
@@ -0,0 +1,53 @@
+<div class="ban-top">
+	<div class="container">
+		<div class="top_nav_left">
+			<nav class="navbar navbar-default">
+			  <div class="container-fluid">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+				  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				  </button>
+				</div>
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse menu--shylock" id="bs-example-navbar-collapse-1">
+				  <ul class="nav navbar-nav menu__list">
+					<li class="active menu__item menu__item--current"><a class="menu__link" href="index.php">Inicio <span class="sr-only">(current)</span></a></li>
+					<li class=" menu__item"><a class="menu__link" href="about.html">Categorias</a></li>
+					<li class=" menu__item"><a class="menu__link" href="contact.html">Todos</a></li>
+					<li class=" menu__item"><a class="menu__link" href="contact.html">Contacto</a></li>
+				  </ul>
+				</div>
+			  </div>
+			</nav>	
+		</div>
+		<div class="top_nav_right">
+			<div class="wthreecartaits wthreecartaits2 cart cart box_1"> 
+						 
+
+					<div class="row">
+					<div class="col-md-6">
+						<form action="#" method="post" class="last"> 
+						<input type="hidden" name="cmd" value="_cart">
+						<input type="hidden" name="display" value="1">
+						<button class="w3view-cart" type="submit" name="submit" value=""><i class="fa fa-shopping-cart" aria-hidden="true" ><span class="label label-success">12</span></i></button>
+					</form> 
+					</div>
+					<div class="col-md-6">
+						<form action="#" method="post" class="last"> 
+						<input type="hidden" name="cmd" value="_cart">
+						<input type="hidden" name="display" value="1">
+						<button class="w3view-cart" type="submit" name="submit" value=""><i class="fa fa-tree" aria-hidden="true"><span class="label label-success">100</span></i></button>
+					</form> 
+					</div>
+					</div> 
+  
+  
+						</div>
+		</div>
+		<div class="clearfix"></div>
+	</div>
+</div>
