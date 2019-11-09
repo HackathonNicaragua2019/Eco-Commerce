74166376bab2ec5cab3c181f09fd93ebe01427b7
Elier25 <elier.aries@gmail.com>
2019-11-09 17:49:45 -0600

    chore: agregando archivos de plantilla
    Se estan agregando archivos para la plnatilla principal de la plataforma

---
+++ includes/footer.php
@@ -0,0 +1,66 @@
+<!--/grids-->
+<div class="coupons">
+		<div class="coupons-grids text-center">
+			<div class="w3layouts_mail_grid">
+				<div class="col-md-3 w3layouts_mail_grid_left">
+					<div class="w3layouts_mail_grid_left1 hvr-radial-out">
+						<i class="fa fa-truck" aria-hidden="true"></i>
+					</div>
+					<div class="w3layouts_mail_grid_left2">
+						<h3>FREE SHIPPING</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur</p>
+					</div>
+				</div>
+				<div class="col-md-3 w3layouts_mail_grid_left">
+					<div class="w3layouts_mail_grid_left1 hvr-radial-out">
+						<i class="fa fa-headphones" aria-hidden="true"></i>
+					</div>
+					<div class="w3layouts_mail_grid_left2">
+						<h3>24/7 SUPPORT</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur</p>
+					</div>
+				</div>
+				<div class="col-md-3 w3layouts_mail_grid_left">
+					<div class="w3layouts_mail_grid_left1 hvr-radial-out">
+						<i class="fa fa-shopping-bag" aria-hidden="true"></i>
+					</div>
+					<div class="w3layouts_mail_grid_left2">
+						<h3>MONEY BACK GUARANTEE</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur</p>
+					</div>
+				</div>
+					<div class="col-md-3 w3layouts_mail_grid_left">
+					<div class="w3layouts_mail_grid_left1 hvr-radial-out">
+						<i class="fa fa-gift" aria-hidden="true"></i>
+					</div>
+					<div class="w3layouts_mail_grid_left2">
+						<h3>FREE GIFT COUPONS</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur</p>
+					</div>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+
+		</div>
+</div>
+<!--grids-->
+<!-- footer -->
+<div class="footer">
+	<div class="footer_agile_inner_info_w3l">
+			<div class="agile_newsletter_footer">
+					<div class="col-sm-6 newsleft">
+				<h3>Suscribete para promociones !</h3>
+			</div>
+			<div class="col-sm-6 newsright">
+				<form action="#" method="post">
+					<input type="email" placeholder="Enter your email..." name="email" required="">
+					<input type="submit" value="Suscribirme">
+				</form>
+			</div>
+
+		<div class="clearfix"></div>
+	</div>
+		<p class="copy-right">&copy 2019 Eco-commerce. Todos los derechos reservados | Diseñada por: <a href="#">System.UNI#</a></p>
+	</div>
+</div>
+<!-- //footer -->
---
+++ includes/header.php
@@ -0,0 +1,34 @@
+
+<div class="header-bot">
+	<div class="header-bot_inner_wthreeinfo_header_mid">
+		<div class="col-md-4 logo_agile">
+				<h1><a href="index.php"><img src="imagenes/logotipoeco.png"></a></h1>
+			</div>
+		<div class="col-md-4 header-middle">
+			<form action="busqueda.php" method="get">
+					<input type="search" name="textobuscar" placeholder="Buscar aqui..." required="">
+					<input type="submit" value=" " name="buscar">
+				<div class="clearfix"></div>
+			</form>
+		</div>
+
+		<div class="col-md-4 agileits-social top_content">
+						<ul class="social-nav model-3d-0 footer-social w3_agile_social">
+						        <li class="share">Siguenos en : </li>
+							<li><a href="#" class="facebook">
+						  <div class="front"><i class="fa fa-facebook" aria-hidden="true"></i></div>
+						  <div class="back"><i class="fa fa-facebook" aria-hidden="true"></i></div></a></li>
+						<li><a href="#" class="twitter"> 
+						  <div class="front"><i class="fa fa-twitter" aria-hidden="true"></i></div>
+						  <div class="back"><i class="fa fa-twitter" aria-hidden="true"></i></div></a></li>
+						 <li><a href="#" class="instagram">
+					  <div class="front"><i class="fa fa-instagram" aria-hidden="true"></i></div>
+					  <div class="back"><i class="fa fa-instagram" aria-hidden="true"></i></div></a></li>
+					   <li><a href="#" class="pinterest">
+					  <div class="front"><i class="fa fa-youtube" aria-hidden="true"></i></div>
+					  <div class="back"><i class="fa fa-youtube" aria-hidden="true"></i></div></a></li>
+														</ul>
+		</div>
+		<div class="clearfix"></div>
+	</div>
+</div>
