bd38106b0ff2b2aa9ac16a36cb10edd24f2f6eb8
Elier25 <elier.aries@gmail.com>
2019-11-10 02:44:54 -0600

    refactor: cambios en la pagina principal
    Modificaciones en la pagina principal

--- index.php
+++ index.php
@@ -44,18 +44,18 @@ $res = $conexion->LeerProductos();
 		   <div class="col-md-3 products-left">
 			
 			<div class="community-poll">
-				<h4>Community Poll</h4>
+				<h4>Categorias</h4>
 				<div class="swit form">	
 					<form>
-					<div class="check_box"> <div class="radio"> <label><input type="radio" name="radio" checked=""><i></i>More convenient for shipping and delivery</label> </div></div>
-					<div class="check_box"> <div class="radio"> <label><input type="radio" name="radio"><i></i>Lower Price</label> </div></div>
-					<div class="check_box"> <div class="radio"> <label><input type="radio" name="radio"><i></i>Track your item</label> </div></div>
-					<div class="check_box"> <div class="radio"> <label><input type="radio" name="radio"><i></i>Bigger Choice</label> </div></div>
-					<div class="check_box"> <div class="radio"> <label><input type="radio" name="radio"><i></i>More colors to choose</label> </div></div>	
-					<div class="check_box"> <div class="radio"> <label><input type="radio" name="radio"><i></i>Secured Payment</label> </div></div>
-					<div class="check_box"> <div class="radio"> <label><input type="radio" name="radio"><i></i>Money back guaranteed</label> </div></div>	
-					<div class="check_box"> <div class="radio"> <label><input type="radio" name="radio"><i></i>Others</label> </div></div>		
-					<input type="submit" value="SEND">
+					<div class="check_box"> <div class="radio"> <label><input type="radio" name="radio" checked=""><i></i>Electronica</label> </div>
+				   </div>
+				   <div class="check_box"> <div class="radio"> <label><input type="radio" name="radio"><i></i>Ropa</label> </div>
+				   </div>
+				   <div class="check_box"> <div class="radio"> <label><input type="radio" name="radio"><i></i>Calzado</label> </div>
+				   </div>	
+				    <div class="check_box"> <div class="radio"> <label><input type="radio" name="radio"><i></i>Electrodomesticos</label> </div>
+				   </div>	
+				   <input type="submit" value="Ver">
 					</form>
 				</div>
 			</div>
