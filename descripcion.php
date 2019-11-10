d48900df521c3ac3bc9684f3171bd28fba7cbef0
Elier25 <elier.aries@gmail.com>
2019-11-09 18:52:19 -0600

    feat: Ver la descripción de un producto
    Se muestra la descripción completa de un producto, ademas de las opiniones de los compradores, calificaciones

---
+++ descripcion.php
@@ -0,0 +1,159 @@
+<?php
+require_once('Conexion.php');
+$res = $conexion->LeerProductos();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Eco-commerce</title>
+<!--/tags -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Plataforma de comercio electronico en Nicaragua" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+		function hideURLbar(){ window.scrollTo(0,1); } </script>
+<?php include('includes/estilos.php');?>
+</head>
+<body>
+<!-- header -->
+<div class="header" id="home">
+	<div class="container">
+		<ul>
+		    <li> <a href="#" data-toggle="modal" data-target="#myModal"><i class="fa fa-unlock-alt" aria-hidden="true"></i> Entrar </a></li>
+			<li> <a href="#" data-toggle="modal" data-target="#myModal2"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Registrarse </a></li>
+			<li><i class="fa fa-phone" aria-hidden="true"></i> Llamar : 01234567898</li>
+			<li><i class="fa fa-envelope-o" aria-hidden="true"></i> <a href="mailto:info@example.com">info@example.com</a></li>
+		</ul>
+	</div>
+</div>
+<!-- //header -->
+<!-- header-bot -->
+<?php include('includes/header.php');?>
+<!-- //header-bot -->
+<!-- banner -->
+<?php include('includes/menu.php');?>
+<!-- //banner-top -->
+<?php include('includes/modales.php');?>
+
+<!-- banner -->
+
+	<div class="container">
+		<div class="top_nav_left">
+			<nav class="navbar navbar-default">
+			  <div class="container-fluid">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+				  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				  </button>
+				</div>
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse menu--shylock" id="bs-example-navbar-collapse-1">
+				  <ul class="nav navbar-nav menu__list">
+					<li class="active menu__item"><a class="menu__link" href="index.html">Home <span class="sr-only">(current)</span></a></li>
+					<li class=" menu__item"><a class="menu__link" href="about.html">About</a></li>
+					<li class="dropdown menu__item">
+						<a href="#" class="dropdown-toggle menu__link" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Men's wear <span class="caret"></span></a>
+							<ul class="dropdown-menu multi-column columns-3">
+								<div class="agile_inner_drop_nav_info">
+									<div class="col-sm-6 multi-gd-img1 multi-gd-text ">
+										<a href="mens.html"><img src="images/top2.jpg" alt=" "/></a>
+									</div>
+									<div class="col-sm-3 multi-gd-img">
+										<ul class="multi-column-dropdown">
+											<li><a href="mens.html">Clothing</a></li>
+											<li><a href="mens.html">Wallets</a></li>
+											<li><a href="mens.html">Footwear</a></li>
+											<li><a href="mens.html">Watches</a></li>
+											<li><a href="mens.html">Accessories</a></li>
+											<li><a href="mens.html">Bags</a></li>
+											<li><a href="mens.html">Caps & Hats</a></li>
+										</ul>
+									</div>
+									<div class="col-sm-3 multi-gd-img">
+										<ul class="multi-column-dropdown">
+											<li><a href="mens.html">Jewellery</a></li>
+											<li><a href="mens.html">Sunglasses</a></li>
+											<li><a href="mens.html">Perfumes</a></li>
+											<li><a href="mens.html">Beauty</a></li>
+											<li><a href="mens.html">Shirts</a></li>
+											<li><a href="mens.html">Sunglasses</a></li>
+											<li><a href="mens.html">Swimwear</a></li>
+										</ul>
+									</div>
+									<div class="clearfix"></div>
+								</div>
+							</ul>
+					</li>
+					<li class="dropdown menu__item">
+						<a href="#" class="dropdown-toggle menu__link" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Women's wear <span class="caret"></span></a>
+							<ul class="dropdown-menu multi-column columns-3">
+								<div class="agile_inner_drop_nav_info">
+									<div class="col-sm-3 multi-gd-img">
+										<ul class="multi-column-dropdown">
+											<li><a href="womens.html">Clothing</a></li>
+											<li><a href="womens.html">Wallets</a></li>
+											<li><a href="womens.html">Footwear</a></li>
+											<li><a href="womens.html">Watches</a></li>
+											<li><a href="womens.html">Accessories</a></li>
+											<li><a href="womens.html">Bags</a></li>
+											<li><a href="womens.html">Caps & Hats</a></li>
+										</ul>
+									</div>
+									<div class="col-sm-3 multi-gd-img">
+										<ul class="multi-column-dropdown">
+											<li><a href="womens.html">Jewellery</a></li>
+											<li><a href="womens.html">Sunglasses</a></li>
+											<li><a href="womens.html">Perfumes</a></li>
+											<li><a href="womens.html">Beauty</a></li>
+											<li><a href="womens.html">Shirts</a></li>
+											<li><a href="womens.html">Sunglasses</a></li>
+											<li><a href="womens.html">Swimwear</a></li>
+										</ul>
+									</div>
+									<div class="col-sm-6 multi-gd-img multi-gd-text ">
+										<a href="womens.html"><img src="images/top1.jpg" alt=" "/></a>
+									</div>
+									<div class="clearfix"></div>
+								</div>
+							</ul>
+					</li>
+					<li class="menu__item dropdown">
+					   <a class="menu__link" href="#" class="dropdown-toggle" data-toggle="dropdown">Short Codes <b class="caret"></b></a>
+								<ul class="dropdown-menu agile_short_dropdown">
+									<li><a href="icons.html">Web Icons</a></li>
+									<li><a href="typography.html">Typography</a></li>
+								</ul>
+					</li>
+					<li class=" menu__item"><a class="menu__link" href="contact.html">Contact</a></li>
+				  </ul>
+				</div>
+			  </div>
+			</nav>	
+		</div>
+		<div class="top_nav_right">
+			<div class="wthreecartaits wthreecartaits2 cart cart box_1"> 
+						<form action="#" method="post" class="last"> 
+						<input type="hidden" name="cmd" value="_cart">
+						<input type="hidden" name="display" value="1">
+						<button class="w3view-cart" type="submit" name="submit" value=""><i class="fa fa-cart-arrow-down" aria-hidden="true"></i></button>
+					</form>  
+  
+						</div>
+		</div>
+		<div class="clearfix"></div>
+	</div>
+	<!-- //new_arrivals --> 
+
+<?php include('includes/footer.php');?>
+<!-- login -->
+<?php include('includes/modal2.php');?>
+<!-- //login -->
+<a href="#home" class="scroll" id="toTop" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+
+<?php include('includes/javas.php');?>
+</body>
+</html>
