fb3cd457b98195bbd138896227fa5a564f9ecee5
Elier25 <elier.aries@gmail.com>
2019-11-09 18:59:18 -0600

    fix: Corrección errores en la pagina descripción
    Se corrigieron dos errores en la pagina descripción del producto

---
+++ desc.php
@@ -0,0 +1,355 @@
+<?php
+ require_once('Conexion.php');
+ $id = $_GET['idProducto'];
+ $r = $conexion->LeerProductosXID($id);
+ $res = mysqli_fetch_assoc($r);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Eco-commerce</title>
+<!--/tags -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Ecommerce Nicaragua" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+		function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- //tags -->
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" />
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<link href="css/easy-responsive-tabs.css" rel='stylesheet' type='text/css'/>
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+
+<!-- //for bootstrap working -->
+<link href="//fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800" rel="stylesheet">
+<link href='//fonts.googleapis.com/css?family=Lato:400,100,100italic,300,300italic,400italic,700,900,900italic,700italic' rel='stylesheet' type='text/css'>
+</head>
+<body>
+	<div class="header" id="home">
+	<div class="container">
+		<ul>
+		    <li> <a href="#" data-toggle="modal" data-target="#myModal"><i class="fa fa-unlock-alt" aria-hidden="true"></i> Entrar </a></li>
+			<li> <a href="#" data-toggle="modal" data-target="#myModal2"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Registrarse </a></li>
+			<li><i class="fa fa-phone" aria-hidden="true"></i> Llamar : 01234567898</li>
+			<li><i class="fa fa-envelope-o" aria-hidden="true"></i> <a href="mailto:info@example.com">info@example.com</a></li>
+		</ul>
+	</div>
+</div>
+<!-- header -->
+<?php include('includes/header.php');?>
+
+<!-- //header-bot -->
+<!-- banner -->
+<div class="ban-top">
+<?php include('includes/menu.php');?>
+</div>
+<!-- //banner-top -->
+<!-- Modal1 -->
+<?php include('includes/modales.php');?>
+  <!-- banner-bootom-w3-agileits -->
+<div class="banner-bootom-w3-agileits">
+	<div class="container">
+	     <div class="col-md-4 single-right-left ">
+			<div class="grid images_3_of_2">
+				<div class="flexslider">
+
+				
+					
+					<ul class="slides">
+						<li data-thumb="<?php echo $res['foto1']; ?>">
+							<div class="thumb-image"> <img src="<?php echo $res['foto1']; ?>" data-imagezoom="true" class="img-responsive"> </div>
+						</li>
+						<li data-thumb="<?php echo $res['foto2']; ?>">
+							<div class="thumb-image"> <img src="<?php echo $res['foto2']; ?>" data-imagezoom="true" class="img-responsive"> </div>
+						</li>	
+						<li data-thumb="<?php echo $res['foto3']; ?>">
+							<div class="thumb-image"> <img src="<?php echo $res['foto3']; ?>" data-imagezoom="true" class="img-responsive"> </div>
+						</li>
+					</ul>
+					<div class="clearfix"></div>
+				</div>	
+			</div>
+		</div>
+		<div class="col-md-8 single-right-left simpleCart_shelfItem">
+					<h3><?php echo $res['nombre']; ?></h3>
+					<p><span class="item_price">$<?php echo $res['precioVenta']; ?></span></p>
+					<div class="rating1">
+						<span class="starRating">
+							<input id="rating5" type="radio" name="rating" value="5">
+							<label for="rating5">5</label>
+							<input id="rating4" type="radio" name="rating" value="4">
+							<label for="rating4">4</label>
+							<input id="rating3" type="radio" name="rating" value="3" checked="">
+							<label for="rating3">3</label>
+							<input id="rating2" type="radio" name="rating" value="2">
+							<label for="rating2">2</label>
+							<input id="rating1" type="radio" name="rating" value="1">
+							<label for="rating1">1</label>
+						</span>
+					</div>
+					<div class="description">
+						<h5>Check delivery, payment options and charges at your location</h5>
+						 <form action="#" method="post">
+						<input type="text" value="Enter pincode" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Enter pincode';}" required="">
+						<input type="submit" value="Check">
+						</form>
+					</div>
+					<div class="color-quality">
+						<div class="color-quality-right">
+							<h5>Quality :</h5>
+							<select id="country1" onchange="change_country(this.value)" class="frm-field required sect">
+								<option value="null">5 Qty</option>
+								<option value="null">6 Qty</option> 
+								<option value="null">7 Qty</option>					
+								<option value="null">10 Qty</option>								
+							</select>
+						</div>
+					</div>
+					<div class="occasional">
+						<h5>Types :</h5>
+						<div class="colr ert">
+							<label class="radio"><input type="radio" name="radio" checked=""><i></i>Casual Shoes</label>
+						</div>
+						<div class="colr">
+							<label class="radio"><input type="radio" name="radio"><i></i>Sneakers </label>
+						</div>
+						<div class="colr">
+							<label class="radio"><input type="radio" name="radio"><i></i>Formal Shoes</label>
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+					<div class="occasion-cart">
+						<div class="snipcart-details top_brand_home_details item_add single-item hvr-outline-out button2">
+															<form action="#" method="post">
+																<fieldset>
+																	<input type="hidden" name="cmd" value="_cart">
+																	<input type="hidden" name="add" value="1">
+																	<input type="hidden" name="business" value=" ">
+																	<input type="hidden" name="item_name" value="Wing Sneakers">
+																	<input type="hidden" name="amount" value="650.00">
+																	<input type="hidden" name="discount_amount" value="1.00">
+																	<input type="hidden" name="currency_code" value="USD">
+																	<input type="hidden" name="return" value=" ">
+																	<input type="hidden" name="cancel_return" value=" ">
+																	<input type="submit" name="submit" value="Add to cart" class="button">
+																</fieldset>
+															</form>
+														</div>
+																			
+					</div>	
+		      </div>
+	 			<div class="clearfix"> </div>
+				<!-- /new_arrivals --> 
+	<div class="responsive_tabs_agileits"> 
+				<div id="horizontalTab">
+						<ul class="resp-tabs-list">
+							<li>Descripcion</li>
+							<li>Opiniones</li>
+							<li>Informacion</li>
+						</ul>
+					<div class="resp-tabs-container">
+					<!--/tab_one-->
+					   <div class="tab1">
+
+							<div class="single_page_agile_its_w3ls">
+							  <h6><?php echo $res['nombre']; ?></h6>
+							   <p><?php echo $res['caracteristicas']; ?></p>
+							   
+							</div>
+						</div>
+						<!--//tab_one-->
+						<div class="tab2">
+							
+							<div class="single_page_agile_its_w3ls">
+								<div class="bootstrap-tab-text-grids">
+									<div class="bootstrap-tab-text-grid">
+										<div class="bootstrap-tab-text-grid-left">
+											<img src="images/t1.jpg" alt=" " class="img-responsive">
+										</div>
+										<div class="bootstrap-tab-text-grid-right">
+											<ul>
+												<li><a href="#">Admin</a></li>
+												<li><a href="#"><i class="fa fa-reply-all" aria-hidden="true"></i> Reply</a></li>
+											</ul>
+											<p>Lorem ipsum dolor sit amet, consectetur adipisicing elPellentesque vehicula augue eget.Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis 
+												suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem 
+												vel eum iure reprehenderit.</p>
+										</div>
+										<div class="clearfix"> </div>
+						             </div>
+									 <div class="add-review">
+										<h4>add a review</h4>
+										<form action="#" method="post">
+												<input type="text" name="Name" required="Name">
+												<input type="email" name="Email" required="Email"> 
+												<textarea name="Message" required=""></textarea>
+											<input type="submit" value="SEND">
+										</form>
+									</div>
+								 </div>
+								 
+							 </div>
+						 </div>
+						   <div class="tab3">
+
+							<div class="single_page_agile_its_w3ls">
+							  <h6>Big Wing Sneakers (Navy)</h6>
+							   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elPellentesque vehicula augue eget nisl ullamcorper, molestie blandit ipsum auctor. Mauris volutpat augue dolor.Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut lab ore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco. labore et dolore magna aliqua.</p>
+							   <p class="w3ls_para">Lorem ipsum dolor sit amet, consectetur adipisicing elPellentesque vehicula augue eget nisl ullamcorper, molestie blandit ipsum auctor. Mauris volutpat augue dolor.Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut lab ore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco. labore et dolore magna aliqua.</p>
+							</div>
+						</div>
+					</div>
+				</div>	
+			</div>
+	<!-- //new_arrivals --> 
+	  	<!--/slider_owl-->
+	
+			<?php
+require_once('Conexion.php');
+$res = $conexion->LeerProductos();
+?>
+
+<div class="container"><br>
+		    <h3 class="wthree_text_info">Productos relacionados</h3>		
+
+		       <?php
+                while($r = mysqli_fetch_array($res)) { 
+                $idproducto =   $r['idproducto'];
+                $nombre =   $r['nombre'];
+                $precio =   $r['precioVenta'];
+                $descripcion =   $r['caracteristicas'];
+                $foto1 =   $r['foto1'];
+                $foto2 =   $r['foto2'];
+                $foto3 =   $r['foto2'];
+                ?> 
+				<div class="col-md-3 product-men">
+					<div class="men-pro-item simpleCart_shelfItem">
+					 <div class="men-thumb-item">
+						<img src="<?php echo $foto1; ?>" alt="" class="pro-image-front">
+						<img src="<?php echo $foto1; ?>" alt="" class="pro-image-back">
+						  <div class="men-cart-pro">
+							<div class="inner-men-cart-pro">
+							 <a href="single.html" class="link-product-add-cart">Ver Detalles</a>
+							</div>
+						  </div>
+							<span class="product-new-top">Nuevo</span>								
+					</div>
+			   <div class="item-info-product ">
+					<h4><a href="single.html"> <?php echo $nombre; ?></a></h4>
+					  <div class="info-product-price">
+							<span class="item_price">C$ <?php echo $precio; ?></span>
+					 </div>
+			       <div class="snipcart-details top_brand_home_details item_add single-item hvr-outline-out button2">
+						<form action="#" method="post">
+							<fieldset>
+							  <input type="submit" name="submit" value="Agregar al Carrito" class="button" />
+							</fieldset>
+						</form>
+					</div>
+																			
+				</div>
+			</div>
+		</div>	<!-- //Final de un Producto --> 
+         <?php } ?>
+	        </div>
+ </div>
+</div>
+	<?php include('includes/footer.php');?>
+
+	<?php include('includes/modal2.php');?>
+
+
+<a href="#home" class="scroll" id="toTop" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+<!-- js -->
+<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+<!-- //js -->
+<script src="js/modernizr.custom.js"></script>
+	<!-- Custom-JavaScript-File-Links --> 
+	<!-- cart-js -->
+	<script src="js/minicart.min.js"></script>
+<script>
+	// Mini Cart
+	paypal.minicart.render({
+		action: '#'
+	});
+
+	if (~window.location.search.indexOf('reset=true')) {
+		paypal.minicart.reset();
+	}
+</script>
+
+	<!-- //cart-js --> 
+	<!-- single -->
+<script src="js/imagezoom.js"></script>
+<!-- single -->
+<!-- script for responsive tabs -->						
+<script src="js/easy-responsive-tabs.js"></script>
+<script>
+	$(document).ready(function () {
+	$('#horizontalTab').easyResponsiveTabs({
+	type: 'default', //Types: default, vertical, accordion           
+	width: 'auto', //auto or any width like 600px
+	fit: true,   // 100% fit in a container
+	closed: 'accordion', // Start closed if in accordion view
+	activate: function(event) { // Callback function if tab is switched
+	var $tab = $(this);
+	var $info = $('#tabInfo');
+	var $name = $('span', $info);
+	$name.text($tab.text());
+	$info.show();
+	}
+	});
+	$('#verticalTab').easyResponsiveTabs({
+	type: 'vertical',
+	width: 'auto',
+	fit: true
+	});
+	});
+</script>
+<!-- FlexSlider -->
+<script src="js/jquery.flexslider.js"></script>
+						<script>
+						// Can also be used with $(document).ready()
+							$(window).load(function() {
+								$('.flexslider').flexslider({
+								animation: "slide",
+								controlNav: "thumbnails"
+								});
+							});
+						</script>
+					<!-- //FlexSlider-->
+<!-- //script for responsive tabs -->		
+<!-- start-smoth-scrolling -->
+<script type="text/javascript" src="js/move-top.js"></script>
+<script type="text/javascript" src="js/jquery.easing.min.js"></script>
+<script type="text/javascript">
+	jQuery(document).ready(function($) {
+		$(".scroll").click(function(event){		
+			event.preventDefault();
+			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+		});
+	});
+</script>
+<!-- here stars scrolling icon -->
+	<script type="text/javascript">
+		$(document).ready(function() {
+			/*
+				var defaults = {
+				containerID: 'toTop', // fading element id
+				containerHoverID: 'toTopHover', // fading element hover id
+				scrollSpeed: 1200,
+				easingType: 'linear' 
+				};
+			*/
+								
+			$().UItoTop({ easingType: 'easeOutQuart' });
+								
+			});
+	</script>
+<!-- //here ends scrolling icon -->
+
+<!-- for bootstrap working -->
+<script type="text/javascript" src="js/bootstrap.js"></script>
+</body>
+</html>
